6b310c68dcec68e284b462568048640185563278
opener <opener@ubuntu.dlink>
2025-09-17 12:19:08 +0000

    prome.rb

--- lib/prome.rb
+++ lib/prome.rb
@@ -16,9 +16,9 @@ module Prome
     # rails
     unless defined?(::Sidekiq) && ::Sidekiq.server?
       r.counter(:rails_requests_total, docstring: "A counter of the total number of HTTP requests rails processed.")
-      r.histogram(:rails_request_duration_seconds, "A histogram of the response latency.")
-      r.histogram(:rails_view_runtime_seconds, "A histogram of the view rendering time.")
-      r.histogram(:rails_db_runtime_seconds, "A histogram of the activerecord execution time.")
+      r.histogram(:rails_request_duration_seconds, docstring: "A histogram of the response latency.")
+      r.histogram(:rails_view_runtime_seconds, docstring: "A histogram of the view rendering time.")
+      r.histogram(:rails_db_runtime_seconds, docstring: "A histogram of the activerecord execution time.")
     end
 
     # sidekiq
--- lib/prome/version.rb
+++ lib/prome/version.rb
@@ -1,3 +1,3 @@
 module Prome
-  VERSION = "0.2.3"
+  VERSION = "0.2.4"
 end
