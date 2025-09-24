fc595bf27bd3c19b8be67b7ae73b9e707209149d
opener <opener@ubuntu.dlink>
2025-09-24 17:24:38 +0000

    DataStore

--- lib/prome/data_store.rb
+++ lib/prome/data_store.rb
@@ -1,12 +1,11 @@
 require 'prometheus/client/data_stores/direct_file_store'
 
 module Prome
-    class DataStore < Prometheus::Client::DataStores::DirectFileStore
-        klass = self.const_get(:MetricStore)
-        MetricStore = Class.new(klass) do
-            private def store_key(labels)
-                labels.to_a.sort.map{|k,v| "#{CGI::escape(k.to_s)}=#{CGI::escape(v.to_s)}"}.join('&')
-            end
-        end
+    MetricStore = Prometheus::Client::DataStores::DirectFileStore.const_get(:MetricStore)
+    MetricStore.define_method(:store_key) do |labels|
+        labels.to_a.sort.map{|k,v| "#{CGI::escape(k.to_s)}=#{CGI::escape(v.to_s)}"}.join('&')
     end
+    MetricStore.send(:private, :store_key)
+    DataStore = Class.new(Prometheus::Client::DataStores::DirectFileStore)
+    DataStore.const_set(:MetricStore, MetricStore)
 end
